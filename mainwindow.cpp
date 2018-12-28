a2b222d4dc3e8867aaec67f82c141b1dc78627e1
wanglichao <wanglc_breeze@sina.com>
2018-12-28 08:54:54 +0800

    add ku

---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+﻿#include <QMessageBox>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    proc = new QProcess;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_helpPushButton_clicked()
+{
+    /*
+     * 注意：读者自行编译运行时，需要将exe改为"./release/assistant.exe"。
+     * 因为项目路径与发布的实际exe路径差了release.
+     * 将params改为./assistant/assistant.qhc。因为项目路径与发布的实际qhc路径差了前后级。
+     */
+    QString exe("./release/assistant.exe");
+    QStringList params;
+    params << "-collectionFile" << "./assistant/assistant.qhc";
+    proc->start(exe, params);
+    if (!proc->waitForStarted()) {
+        QMessageBox::critical(0, QObject::tr("Tips"),
+            QObject::tr("Unable to launch Qt Assistant (%1)").arg(exe));
+    }
+}
